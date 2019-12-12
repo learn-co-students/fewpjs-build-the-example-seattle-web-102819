e7197be0792be0fdcc6cc066f7fcc98943a867c3
moliver4 <tifftang22@gmail.com>
2019-12-12 15:08:36 -0800

    Done.

--- main.js
+++ main.js
@@ -5,6 +5,42 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+const hearts = document.querySelectorAll(".like-glyph");
+for (const heart of hearts) {
+  heart.addEventListener("click", heartClicked)
+}
+
+function heartClicked(e) {
+  let heartnode = e.target
+  mimicServerCall("URL")
+    .then (function(serverMessage) {
+      changeHeart(heartnode)
+      return serverMessage
+    })
+    .catch(function(error) {
+      document.getElementById("modal").className = "";
+      console.log(error.message);
+    });
+}
+
+function changeHeart(targetNode) {
+  let h = targetNode.textContent;
+
+  if (h === FULL_HEART) {
+    targetNode.textContent = EMPTY_HEART;
+    targetNode.style.color = "";
+  } else {
+    targetNode.textContent = FULL_HEART;
+    targetNode.style.color = "red";
+  }
+}
+
+
+
+
+
+
+
 
 
 //------------------------------------------------------------------------------
